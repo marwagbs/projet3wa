419f65c64d88ce914fdc9ef8f25576bfef5377b1
marwagbs <90385255+marwagbs@users.noreply.github.com>
2023-10-05 12:54:44 +0200

    Create about.php

---
+++ about.php
@@ -0,0 +1,12 @@
+<?php
+//affichage de la page about
+//dépendance
+require_once "services/Session.php";
+require_once 'services/utils.php';
+Session::start();
+
+
+$pagePath = 'about';
+
+// Rendu de la vue
+render(__DIR__."/views/$pagePath");
